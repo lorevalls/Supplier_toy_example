d7a4ede6f651a08d941480f0f1040c27eeb524e0
lorevalls <115902446+lorevalls@users.noreply.github.com>
2024-08-28 18:49:28 +0000

    feature: added parameter fit

--- BN_supplier.r
+++ BN_supplier.r
@@ -5,7 +5,16 @@ workdf$supplier_name<-as.factor(workdf$supplier_name)
 workdf$products_sold<-as.numeric(workdf$products_sold)
 workdf$num_employees<-as.numeric(workdf$num_employees)
 workdf$num_transports<-as.numeric(workdf$num_transports)
+workdf$income<-as.numeric(workdf$income)
 
 # structure learning
 net<-hc(workdf, score = 'bic-cg')
 plot(net)
+
+# parameter leanrig
+net_coef<-bn.fit(net, workdf)
+print(net_coef)
+##bn.fit.barchart(net_coef$income)
+
+# beilif propagation
+
